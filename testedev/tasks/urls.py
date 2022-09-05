bf73b53bf18aab760426748787fdfeb0148c25f3
Renato Cesar Arantes <renato.arantes@hegv.cesp.ses.rj.gov.br>
2022-09-05 11:15:43 -0300

    adicionando banco de dados

--- testedev/tasks/urls.py
+++ testedev/tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('testedev/', views.testedev),
     path('', views.taskslist, name='tasks-list'),
     path('task/<int:id>', views.taskView, name="task-view"),
+    path('newtask/', views.newTask, name="new-task"),
     path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
