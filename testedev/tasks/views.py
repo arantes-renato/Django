f9bc7e47416cc1667d880d376e2ed6b7c26006a5
Renato Cesar Arantes <renato.arantes@hegv.cesp.ses.rj.gov.br>
2022-09-13 15:37:09 -0300

    concluir tarefa

--- testedev/tasks/views.py
+++ testedev/tasks/views.py
@@ -72,5 +72,18 @@ def deleteTask(request, id):
 
     return redirect('/')
 
+@login_required
+def changeStatus(request, id):
+    task = get_object_or_404(Task, pk=id)
+
+    if(task.done == "doing"):
+        task.done = 'done'
+    else:
+        task.done = 'doing'
+    
+    task.save()
+
+    return redirect('/')
+
 def yourName(request, name):
     return render(request,'tasks/yourname.html', {'name': name})
